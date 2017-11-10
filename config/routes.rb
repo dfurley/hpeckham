c36c0790b1ff6daf1921301dac8e12af7a59671f
David Furley <furley.david@gmail.com>
2017-11-10 11:43:56 +0000

    cleaned contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home' 
   get 'about', to: 'pages#about'
   get 'gallery', to: 'pages#gallery'
-  resources :contacts
-  get 'contact', to: 'contacts#new'
+  resources :contacts, only: :create
+  get 'contact', to: 'contacts#new', as: 'new_contact'
 end
