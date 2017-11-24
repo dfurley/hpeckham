d0cf93cd12e6a8495b70de7985e3c0e6bb037fc2
David Furley <furley.david@gmail.com>
2017-11-24 11:33:24 +0000

    updated to hugos email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'furley.david@gmail.com'
+  default to: 'peckhamhugo@gmail.com'
   def contact_email(name, email, subject, body)
     @name = name
     @email = email
